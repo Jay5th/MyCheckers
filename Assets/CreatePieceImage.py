985385554c203bbc8399e5502d97dc8ac1181829
Japheth Varlack <japhethv1229@gmail.com>
2024-05-05 20:28:20 -0400

    fixed a bug in image creation

--- Assets/CreatePieceImage.py
+++ Assets/CreatePieceImage.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageTk
 
 
 def create_piece_image(piece_name: str, piece_radius: int):
-    assets_path = "C:\\Users\\japhe\\OneDrive\\Desktop\\Program Projects\\GitHubCheckers\\Assets\\"
+    assets_path = "Assets\\"
     image_path = assets_path + f"{piece_name}.png"
     image = Image.open(image_path)
     image = image.resize((2 * piece_radius, 2 * piece_radius))
